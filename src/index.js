e0c128057b63829c7a93576d3b27beb5837bb2c6
muhammad Rusdi <muhamamdrusdi@icloud.com>
2018-11-18 11:52:15 +0800

    updated container

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Row from './components/Grid/Row'
+import Container from './components/Container'
 import Col from './components/Grid/Col'
 import Badge from './components/Badge'
 
-export { Badge, Row, Col }
+export { Badge, Container, Row, Col }
